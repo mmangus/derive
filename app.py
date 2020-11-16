63131d285d6f56767ca12c6036acf81af2fb34b9
michael mangus <mmangus@gmail.com>
2020-11-15 18:05:11 -0800

    test out hints

--- app.py
+++ app.py
@@ -10,35 +10,36 @@ from flask import (
 from flask_bootstrap import Bootstrap
 from folium import Map
 
-# TODO temporary, see locations.py
 import locations
+
+# TODO temporary, see locations.py
 LOCATIONS = locations.load()
+DEFAULT_MAP_KWARGS = {
+    "tiles": "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.png",
+    "attr": "Map tiles by <a href='http://stamen.com'>Stamen Design</a>, <a href="
+            "'http://creativecommons.org/licenses/by/3.0'>CC BY 3.0</a> &mdash; Map data &copy;"
+            " <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors.",
+    "zoom_control": False,
+    "scrollWheelZoom": False,
+    "doubleClickZoom": False,
+    "dragging": False,
+    "no_touch": True,
+    "control_scale": True,
+    "width": "100",
+    "height": "100",
+}
 
 app = Flask(__name__)
 Bootstrap(app)
 
-DEFAULT_MAP_KWARGS = {
-    'tiles': "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.png",
-    'attr': 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href='
-            '"http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy;'
-            ' <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors.',
-    'zoom_control': False,
-    'scrollWheelZoom': False,
-    'doubleClickZoom': False,
-    'dragging': False,
-    'no_touch': True,
-    'control_scale': True,
-    'width': '100',
-    'height': '100',
-}
 
-@app.route('/')
+@app.route("/")
 def homepage():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-# @app.route('/play/')  TODO handle this later
-@app.route('/play/<loc_id>/')
+# @app.route("/play/")  TODO handle this later
+@app.route("/play/<loc_id>/")
 def play(loc_id=None):
     # TODO:
     #  - hook this up to a DB model
@@ -47,17 +48,30 @@ def play(loc_id=None):
     # 
     # if not loc_id:
     #    loc_id = random.choice(range(len(LOCATIONS)))
-    #    return redirect(url_for('play', loc_id=loc_id))
+    #    return redirect(url_for("play", loc_id=loc_id))
     
     try:
         loc_id = int(loc_id)
     except ValueError:
         abort(400)
-    _, coordinates = LOCATIONS[int(loc_id)]
+    place_data  = LOCATIONS[int(loc_id)]
+    place_name = place_data.name
+    hints = [
+        ("Hemisphere (latitude)", place_data.hemisphere_lat),
+        ("Hemisphere (longitude)", place_data.hemisphere_lon),
+        ("Continent (ocean for islands)", place_data.continent_or_cean),
+        ("Country", place_data.country_name),
+    ]
     next_loc = loc_id + 1 if loc_id < len(LOCATIONS) else 0
     map = Map(
-        location=coordinates,
+        location=place_data.coordinates,
         **DEFAULT_MAP_KWARGS
     )._repr_html_()  # TODO override to e.g. change placeholder etc
 
-    return render_template('play.html', map=map, next_loc=next_loc)
+    return render_template(
+        "play.html",
+        map=map,
+        next_loc=next_loc,
+        placen_name=place_name,
+        hints=hints,
+    )
--- locations.py
+++ locations.py
@@ -1,10 +1,10 @@
+from io import BytesIO
 import os
 import zipfile
 
+from iso3166 import countries
 import pandas as pd
 import requests
-
-from io import BytesIO
 
 # TODO:
 #  - make a DB model for this
@@ -33,6 +33,15 @@ GEONAMES_COLUMNS = [
   'modification date',
 ]
 
+
+def cc_to_name(cc):
+  try:
+    country = countries.get(cc)
+  except KeyError:
+    return 'Geonames value was not a ISO 3166 country code'
+  return country.apolitical_name
+
+
 def geonames_to_file(n=100):
     geonames_dumpfile = BytesIO(
         requests.get(
@@ -41,14 +50,20 @@ def geonames_to_file(n=100):
     )
     cities = zipfile.ZipFile(geonames_dumpfile).open("cities15000.txt") 
     cities_df = pd.read_csv(cities, sep='\t', names=GEONAMES_COLUMNS)
-    cities_df['coordinates'] = cities_df[['latitude', 'longitude']].values.tolist()
-    top_n = cities_df.sort_values('population', ascending=False)[[
-        'name', 'coordinates'
-    ]][:n].to_json('cities.json')
+    cities_df["coordinates"] = cities_df[["latitude", "longitude"]].values.tolist()
+    cities_df["continent_or_ocean"] = cities_df["timezone"].apply(lambda tz: tz.split("/")[0])
+    # TODO fuzzy reckoning on hemispheres; what"s the lon equivalent of "equatorial?"
+    cities_df["hemisphere_lat"] = cities_df["latitude"].apply(lambda lat: "North" if lat >= 0 else "South")
+    cities_df["hemisphere_lon"] = cities_df["longitude"].apply(lambda lon: "East" if lon >= 0 else "West")
+    cities_df["country_name"] = cities_df["country code"].apply(lambda cc: cc_to_name(cc))
+    top_n = cities_df.sort_values("population", ascending=False)[[
+        "name", "coordinates", "continent_or_ocean", "hemisphere_lat", "hemisphere_lon", "country_name"
+    ]][:n].to_json("cities.json")
 
 
 def load():
     if not os.path.exists("cities.json"):
         geonames_to_file()
     # could just use json module but we've got pandas imported already
-    return pd.read_json("cities.json").values
+    cities_df = pd.read_json("cities.json")
+    return cities_df.to_records()
